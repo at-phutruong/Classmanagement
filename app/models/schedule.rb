af5b409d6c796beaca0554e9cd28fe565a38df54
Truong Van Phu <phugodness@gmail.com>
2016-10-16 00:17:46 +0700

    fix some error on create schedule

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -10,12 +10,18 @@ class Schedule < ApplicationRecord
       schedules = Schedule.all
       # binding.pry
       start_time = start.split(":")
+      start_time = (start_time[0] + start_time[1]).to_i
       finish_time = finish.split(":")
+      finish_time = (finish_time[0] + finish_time[1]).to_i
       schedules.each do |schedule|
         schedule_start_time = schedule.start.split(":")
+        schedule_start_time = (schedule_start_time[0] + schedule_start_time[1]).to_i
         schedule_finish_time = schedule.finish.split(":")
+        schedule_finish_time = (schedule_finish_time[0] + schedule_finish_time[1]).to_i
         # binding.pry
-        errors.add(:hour, 'has already taken') if classroom_id == schedule.classroom_id && date == schedule.date && ((schedule_start_time[0].to_i...schedule_finish_time[0].to_i) === (start_time[0].to_i)||(schedule_start_time[0].to_i...schedule_finish_time[0].to_i) === (finish_time[0].to_i))
+        if classroom_id == schedule.classroom_id && date == schedule.date && ((schedule_start_time...schedule_finish_time) === (start_time)||(schedule_start_time + 1...schedule_finish_time) === (finish_time))
+          errors.add(:hour, 'has already taken')
+        end
       end
     end
 
